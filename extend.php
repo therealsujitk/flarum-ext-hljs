7b694de24942823079962167ea69b5f5d533c19e
Golden <35162802+Littlegolden@users.noreply.github.com>
2021-03-04 15:53:15 +0800

    Add more code language

--- extend.php
+++ extend.php
@@ -17,7 +17,7 @@ use Illuminate\Contracts\Events\Dispatcher;
 return [
     (new Extend\Frontend('forum'))
         ->js(__DIR__.'/js/dist/forum.js')
-        ->css(__DIR__ . '/resources/less/forum.less'),      
+        ->css(__DIR__ . '/resources/less/colang.min.less'),      
     (new Extend\Frontend('admin'))
         ->js(__DIR__.'/js/dist/admin.js'),
     new Extend\Locales(__DIR__ . '/resources/locale'),
